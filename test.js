0d3480601f81e18e19ef39c1811f91318ea01a23
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-26 01:13:52 +0200

    extend test cases

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import R from 'ramda';
 import contract from './index';
 
 test('basic', t => t.is(
@@ -12,3 +13,29 @@ test('curried', t => t.is(
 ));
 
 test('throws', t => t.throws(() => { contract('input', String, 2); }, TypeError));
+
+// sync data flow
+const prefixEslintPlugin = R.pipe(
+  contract('pluginName', String),
+  R.concat('eslint-plugin-')
+);
+
+test('pipe', t => t.is(
+  prefixEslintPlugin('import'),
+  'eslint-plugin-import'
+));
+test('pipe throws', t => t.throws(() => { prefixEslintPlugin(2); }, TypeError));
+
+// async data flow
+const resolve = Promise.resolve.bind(Promise);
+const prefixEslintPluginAsync = R.pipeP(resolve,
+  contract('pluginName', String),
+  R.concat('eslint-plugin-')
+);
+
+test('pipe async', async t => t.is(
+  await prefixEslintPluginAsync('import'),
+  'eslint-plugin-import'
+));
+
+test('pipe async rejects', t => t.throws(prefixEslintPluginAsync(2), TypeError));
